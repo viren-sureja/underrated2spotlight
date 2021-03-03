a84b1257cf1b1d680250e67d930aa7023814d3de
viren sureja <virensureja3@gmail.com>
2021-03-04 03:01:09 +0530

    initiated main server file

---
+++ backend/app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const placesRoutes = require('./routes/places-routes');
+const usersRoutes = require('./routes/users-routes');
+
+const app = express();
+
+app.use('/api/places', placesRoutes);
+app.use('/api/users', usersRoutes);
+
+app.use((error, req, res, next) => {
+	if (res.headerSent) {
+		return next(error);
+	}
+	res.status(error.code || 500);
+	res.json({ message: error.message || 'An unknown error occured!' });
+});
+
+app.listen(5000, () => {
+	console.log('listening on 5000...');
+});

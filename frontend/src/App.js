fe2c382e2b972568499bd62eacc3071a6717b5b3
viren sureja <virensureja3@gmail.com>
2021-03-03 16:32:23 +0530

    route added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,6 +11,7 @@ import Users from './user/pages/Users';
 import NewPlace from './places/pages/NewPlace';
 import MainNavigation from './shared/components/Navigation/MainNavigation.js';
 import UserPlaces from './places/pages/UserPlaces';
+import UpdatePlace from './places/pages/UpdatePlace';
 
 function App() {
 	return (
@@ -21,11 +22,14 @@ function App() {
 					<Route path="/" exact>
 						<Users />
 					</Route>
+					<Route path="/:userId/places" exact>
+						<UserPlaces />
+					</Route>
 					<Route path="/places/new" exact>
 						<NewPlace />
 					</Route>
-					<Route path="/:userId/places" exact>
-						<UserPlaces />
+					<Route path="/places/:placeId" exact>
+						<UpdatePlace />
 					</Route>
 					<Redirect to="/" />
 				</Switch>

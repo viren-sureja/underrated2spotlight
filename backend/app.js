aae34025060992cce0b69faaa0a44436cf19795a
viren sureja <virensureja3@gmail.com>
2021-03-09 15:29:32 +0530

    updated CORS header for token

--- backend/app.js
+++ backend/app.js
@@ -17,7 +17,7 @@ app.use(bodyParser.json());
 
 app.use('/uploads/images', express.static(path.join('uploads', 'images')));
 
-// header to attach to avoid CORS
+// header attached to avoid CORS
 app.use((req, res, next) => {
 	res.setHeader('Access-Control-Allow-Origin', '*');
 	res.setHeader(

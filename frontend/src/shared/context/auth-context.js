b1533d40406024c2f05a347cd4da48c3b4cbde3f
viren sureja <virensureja3@gmail.com>
2021-03-09 15:44:21 +0530

    added token field

--- frontend/src/shared/context/auth-context.js
+++ frontend/src/shared/context/auth-context.js
@@ -3,8 +3,7 @@ import { createContext } from 'react';
 export const AuthContext = createContext({
 	isLoggedIn: false,
 	userId: null,
+	token: null,
 	login: () => {},
-	logout: () => {
-		console.log('logout');
-	},
+	logout: () => {},
 });
